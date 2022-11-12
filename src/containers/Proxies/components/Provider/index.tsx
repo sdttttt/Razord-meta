50da7746c6d45c35ed3887434dc811a91cc8368a
GyDi <zzzgydi@gmail.com>
2022-11-12 16:21:00 +0800

    chore: fix subscription info style

--- src/containers/Proxies/components/Provider/index.tsx
+++ src/containers/Proxies/components/Provider/index.tsx
@@ -1,77 +1,114 @@
-import { useMemo } from 'react'
+import { useMemo } from "react";
+import { Card, Tag, Icon, Loading } from "@components";
+import { Progress } from "@components/Progress";
+import { compareDesc } from "@containers/Proxies";
+import { Proxy } from "@containers/Proxies/components/Proxy";
+import { fromNow } from "@lib/date";
+import { useVisible } from "@lib/hook";
+import { Provider as IProvider, Proxy as IProxy } from "@lib/request";
+import { useClient, useI18n, useProxyProviders } from "@stores";
+import "./style.scss";
 
-import { Card, Tag, Icon, Loading } from '@components'
-import { Progress } from '@components/Progress'
-import { compareDesc } from '@containers/Proxies'
-import { Proxy } from '@containers/Proxies/components/Proxy'
-import { fromNow } from '@lib/date'
-import { useVisible } from '@lib/hook'
-import { Provider as IProvider, Proxy as IProxy } from '@lib/request'
-import { useClient, useI18n, useProxyProviders } from '@stores'
-
-import './style.scss'
 interface ProvidersProps {
-    provider: IProvider
+    provider: IProvider;
 }
 
-export function Provider (props: ProvidersProps) {
-    const { update } = useProxyProviders()
-    const { translation, lang } = useI18n()
-    const client = useClient()
+export function Provider(props: ProvidersProps) {
+    const { update } = useProxyProviders();
+    const { translation, lang } = useI18n();
+    const client = useClient();
 
-    const { provider } = props
-    const { t } = translation('Proxies')
+    const { provider } = props;
+    const { t } = translation("Proxies");
 
-    const { visible, hide, show } = useVisible()
+    const { visible, hide, show } = useVisible();
 
-    function handleHealthChech () {
-        show()
-        client.healthCheckProvider(provider.name).then(async () => await update()).finally(() => hide())
+    function handleHealthChech() {
+        show();
+        client
+            .healthCheckProvider(provider.name)
+            .then(async () => await update())
+            .finally(() => hide());
     }
     const expireStr = useMemo(() => {
-        const expire = provider.subscriptionInfo?.Expire ? new Date(provider.subscriptionInfo.Expire * 1000) : new Date(0)
-        const getYear = expire.getFullYear().toString() + '-'
-        const getMonth = (expire.getMonth() + 1 < 10 ? '0' + (expire.getMonth() + 1).toString() : expire.getMonth() + 1).toString() + '-'
-        const getDate = expire.getDate().toString() + ' '
-        return (getYear + getMonth + getDate)
-    }, [provider.subscriptionInfo])
-    function handleUpdate () {
-        show()
-        client.updateProvider(provider.name).then(async () => await update()).finally(() => hide())
+        const expire = provider.subscriptionInfo?.Expire
+            ? new Date(provider.subscriptionInfo.Expire * 1000)
+            : new Date(0);
+        const getYear = expire.getFullYear().toString() + "-";
+        const getMonth =
+            (expire.getMonth() + 1 < 10
+                ? "0" + (expire.getMonth() + 1).toString()
+                : expire.getMonth() + 1
+            ).toString() + "-";
+        const getDate = expire.getDate().toString() + " ";
+        return getYear + getMonth + getDate;
+    }, [provider.subscriptionInfo]);
+    function handleUpdate() {
+        show();
+        client
+            .updateProvider(provider.name)
+            .then(async () => await update())
+            .finally(() => hide());
     }
 
     const proxies = useMemo(() => {
-        return (provider.proxies as IProxy[]).slice().sort((a, b) => -1 * compareDesc(a, b))
-    }, [provider.proxies])
+        return (provider.proxies as IProxy[])
+            .slice()
+            .sort((a, b) => -1 * compareDesc(a, b));
+    }, [provider.proxies]);
+
     return (
         <Card className="proxy-provider">
             <Loading visible={visible} />
-            <div className="flex flex-col justify-between md:(flex-row items-center) ">
-                <div className="flex items-center">
-                    <span className="mr-6">{ provider.name }</span>
-                    <Tag>{ provider.vehicleType }</Tag>
-                    <Tag className="rule-provider-behavior">{ provider.proxies.length }</Tag>
-                    { (provider.subscriptionInfo?.Expire !== 0) && <Tag className="rule-provider-expire">Expire: { expireStr }</Tag> }
-                    { provider.subscriptionInfo && <Progress subscriptionInfo={provider.subscriptionInfo}></Progress> }
+            <div className="flex flex-col justify-between md:(flex-row items-center)">
+                <div className="flex items-center flex-wrap gap-4">
+                    <span className="mr-2">{provider.name}</span>
+                    <Tag>{provider.vehicleType}</Tag>
+                    <Tag className="rule-provider-behavior m-0">
+                        {provider.proxies.length}
+                    </Tag>
+                    {provider.subscriptionInfo?.Expire !== 0 && (
+                        <Tag className="rule-provider-expire m-0">
+                            Expire: {expireStr}
+                        </Tag>
+                    )}
+                    {provider.subscriptionInfo && (
+                        <Progress
+                            subscriptionInfo={provider.subscriptionInfo}
+                        />
+                    )}
                 </div>
-                <div className="flex pt-3 items-center md:pt-0">
-                    {
-                        provider.updatedAt &&
-                        <span className="text-sm">{ `${t('providerUpdateTime')}: ${fromNow(new Date(provider.updatedAt), lang)}`}</span>
-                    }
-                    <Icon className="cursor-pointer text-red pl-5" type="healthcheck" size={18} onClick={handleHealthChech} />
-                    <Icon className="cursor-pointer pl-5" type="update" size={18} onClick={handleUpdate} />
+
+                <div className="flex pt-3 items-center md:pt-0 flex-none">
+                    {provider.updatedAt && (
+                        <span className="text-sm">{`${t(
+                            "providerUpdateTime"
+                        )}: ${fromNow(
+                            new Date(provider.updatedAt),
+                            lang
+                        )}`}</span>
+                    )}
+                    <Icon
+                        className="cursor-pointer text-red pl-5"
+                        type="healthcheck"
+                        size={18}
+                        onClick={handleHealthChech}
+                    />
+                    <Icon
+                        className="cursor-pointer pl-5"
+                        type="update"
+                        size={18}
+                        onClick={handleUpdate}
+                    />
                 </div>
             </div>
             <ul className="proxies-list">
-                {
-                    proxies.map((p: IProxy) => (
-                        <li key={p.name}>
-                            <Proxy className="proxy-provider-item" config={p} />
-                        </li>
-                    ))
-                }
+                {proxies.map((p: IProxy) => (
+                    <li key={p.name}>
+                        <Proxy className="proxy-provider-item" config={p} />
+                    </li>
+                ))}
             </ul>
         </Card>
-    )
+    );
 }
